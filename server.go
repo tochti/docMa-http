d2014c820b11b1f80e640223ee30eb4f524c6676
Tim Tochtermann <tim.tochtermann@posteo.de>
2015-05-17 21:05:25 +0200

    add static files, serve html files on index route

--- server.go
+++ server.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+  "path"
   "github.com/gin-gonic/gin"
   "github.com/rrawrriw/bebber"
 )
 
 func main() {
   router := gin.Default()
+  htmlDir := path.Join(bebber.GetSettings("BEBBER_PUBLIC"), "html")
+  router.Use(bebber.Serve("/", bebber.LocalFile(htmlDir, false)))
   router.POST("/LoadDir", bebber.LoadDir)
   router.POST("/AddTags", bebber.AddTags)
+  router.Static("/public", bebber.GetSettings("BEBBER_PUBLIC"))
   serverStr := bebber.GetSettings("BEBBER_IP") +":"+
                bebber.GetSettings("BEBBER_PORT")
   router.Run(serverStr)
